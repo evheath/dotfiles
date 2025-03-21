87495b6f733d9a7ad7bbd087382b284e8c96e103
Elliot Heath <elliot@businessfactory.dev>
2025-03-21 11:44:29 -0500

    press esc in telescope to close

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -48,11 +48,11 @@ return { -- Fuzzy Finder (files, lsp, etc)
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
+      defaults = {
+        mappings = {
+          i = { ['<esc>'] = require('telescope.actions').close },
+        },
+      },
       -- pickers = {}
       extensions = {
         ['ui-select'] = {
@@ -64,6 +64,5 @@ return { -- Fuzzy Finder (files, lsp, etc)
     -- Enable Telescope extensions if they are installed
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
-
   end,
 }
