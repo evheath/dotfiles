c9505ea7dd19059674a72dd2b2fa2d0216e97c9a
Elliot Heath <elliot@businessfactory.dev>
2025-03-22 15:45:48 -0500

    feature: close floating terminal when pressing esc

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -18,7 +18,7 @@ vim.keymap.set('n', '<M-j>', '<C-w>j', { desc = 'Move focus to the lower window'
 vim.keymap.set('n', '<M-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
 
 -- Escape help
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR><cmd>fclose<CR>')
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- diagnostic
