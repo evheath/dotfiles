b4c1688cfa7101787ec820d98350eed54d6e29ac
Elliot Heath <elliot@businessfactory.dev>
2025-03-30 08:49:01 -0500

    feature: lualine buffer colors, realative filepath, progress

--- nvim/lua/custom/plugins/lua-line.lua
+++ nvim/lua/custom/plugins/lua-line.lua
@@ -6,11 +6,11 @@ return {
     sections = {
       -- https://github.com/nvim-lualine/lualine.nvim/blob/master/README.md#available-components
       lualine_a = { 'mode' },
-      lualine_b = { 'buffers' },
+      lualine_b = { { 'buffers', use_mode_colors = true } },
       lualine_c = {},
-      lualine_x = { 'diff', 'diagnostics' },
-      lualine_y = { 'location' },
-      lualine_z = { 'mode' },
+      lualine_x = { 'diff', 'diagnostics', { 'filename', path = 1 } },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
     },
   },
 }
