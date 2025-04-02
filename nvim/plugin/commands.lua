dec37888640df67bca6812884c22d49b084e73d2
Elliot Heath <elliot@businessfactory.dev>
2025-04-02 09:39:13 -0500

    feature: RevealInFinder user_command

--- nvim/plugin/commands.lua
+++ nvim/plugin/commands.lua
@@ -1,3 +1,12 @@
 vim.api.nvim_create_user_command('CloseOthers', function()
   vim.cmd 'silent! %bd | e# | bd#'
 end, {})
+
+vim.api.nvim_create_user_command('RevealInFinder', function()
+  local file = vim.fn.expand '%:p'
+  if file ~= '' then
+    vim.fn.system { 'open', '-R', file }
+  else
+    print 'No file to reveal'
+  end
+end, {})
