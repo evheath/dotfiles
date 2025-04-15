b5df5c4f8d74f213629924a3ba405860aa8f8969
Elliot Heath <elliot@businessfactory.dev>
2025-04-15 10:07:38 -0500

    feature: same node

--- nvim/lua/custom/snippets/go.lua
+++ nvim/lua/custom/snippets/go.lua
@@ -4,6 +4,14 @@
 local ls = require 'luasnip'
 local s = ls.snippet
 local i = ls.insert_node
+local f = ls.function_node
+-- given the index of the node, return its same result
+local same = function(index)
+  return f(function(arg)
+    return arg[1]
+  end, { index })
+end
+
 local fmt = require('luasnip.extras.fmt').fmt
 
 -- NOTE: i(0) means exit, otherwise i(1) means first, i(2) means second etc
@@ -25,4 +33,5 @@ ls.add_snippets('go', {
       { i(1), i(2), i(3), i(4), i(0) }
     )
   ),
+  s('log', fmt('log.Printf("{}:%v\\n", {}){}', { same(1), i(1), i(0) })),
 })
