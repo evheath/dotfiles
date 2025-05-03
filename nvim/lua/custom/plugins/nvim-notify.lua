1f88a31f4ae751b11a1b0e46d7c9bd817a7635bf
Elliot Heath <elliot@businessfactory.dev>
2025-05-03 10:01:08 -0500

    noice presets and background colors

---
+++ nvim/lua/custom/plugins/nvim-notify.lua
@@ -0,0 +1,9 @@
+return {
+  'rcarriga/nvim-notify',
+  event = 'VeryLazy', -- or use "UIEnter" or similar to lazy-load
+  config = function()
+    require('notify').setup {
+      background_colour = '#1E1E2E',
+    }
+  end,
+}
