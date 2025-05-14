b19b8c4311a08509a09fd9be3d23b05d0ab4ef6b
Elliot Heath <elliot@businessfactory.dev>
2025-05-14 10:37:31 -0500

    catppuccin bufferline

--- nvim/lua/custom/plugins/catppuccin.lua
+++ nvim/lua/custom/plugins/catppuccin.lua
@@ -1,25 +1,36 @@
 -- https://github.com/catppuccin/nvim
 return {
-  'catppuccin/nvim',
-  lazy = false,
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    require('catppuccin').setup {
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        neotree = true,
-        treesitter = true,
-        noice = true,
-        mini = {
-          enabled = true,
-          indentscope_color = '',
+  {
+    'catppuccin/nvim',
+    lazy = false,
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          neotree = true,
+          treesitter = true,
+          noice = true,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          which_key = true,
         },
-        which_key = true,
-      },
-      transparent_background = true,
-    }
-    vim.cmd 'colorscheme catppuccin-mocha'
-  end,
+        transparent_background = true,
+      }
+      vim.cmd 'colorscheme catppuccin-mocha'
+    end,
+  },
+  {
+    'akinsho/bufferline.nvim',
+    after = 'catppuccin',
+    config = function()
+      require('bufferline').setup {
+        highlights = require('catppuccin.groups.integrations.bufferline').get(),
+      }
+    end,
+  },
 }
