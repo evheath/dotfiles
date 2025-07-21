2e53a16f73ed6793c3beecfb5f5272bbe2166c4c
Elliot Heath <elliot@businessfactory.dev>
2025-07-21 08:47:11 -0500

    erb snippets

--- nvim/plugin/snippets.lua
+++ nvim/plugin/snippets.lua
@@ -56,3 +56,24 @@ ls.add_snippets('go', {
 ls.add_snippets('ruby', {
   s('frozen', fmt('# frozen_string_literal: true{}', { i(0) })),
 })
+
+ls.add_snippets('eruby', {
+  s('comment', fmt('<%# {} %>', { i(0) })),
+  s('output', fmt('<%= {} %>', { i(0) })),
+  s('execute', fmt('<% {} %>', { i(0) })),
+  s('if', fmt('<% if {} %>\n  {}\n<% end %>', { i(1, 'condition'), i(0) })),
+  s('ife', fmt('<% if {} %>\n  {}\n<% else %>\n  {}\n<% end %>', { i(1, 'condition'), i(2), i(0) })),
+  s('elsif', fmt('<% elsif {} %>\n  {}', { i(1, 'condition'), i(0) })),
+  s('unless', fmt('<% unless {} %>\n  {}\n<% end %>', { i(1, 'condition'), i(0) })),
+  s('each', fmt('<% {}.each do |{}| %>\n  {}\n<% end %>', { i(1, 'collection'), i(2, 'item'), i(0) })),
+  s('eachidx', fmt('<% {}.each_with_index do |{}, {}| %>\n  {}\n<% end %>', { i(1, 'collection'), i(2, 'item'), i(3, 'index'), i(0) })),
+  s('link', fmt('<%= link_to "{}", {} %>', { i(1, 'text'), i(0, 'path') })),
+  s('form', fmt('<%= form_with {} do |f| %>\n  {}\n<% end %>', { i(1, 'model: @model'), i(0) })),
+  s('submit', fmt('<%= f.submit "{}", class: "{}" %>', { i(1, 'Submit'), i(0, 'btn btn-primary') })),
+  s('field', fmt('<%= f.{} :{}, class: "{}" %>', { i(1, 'text_field'), i(2, 'attribute'), i(0, 'form-control') })),
+  s('label', fmt('<%= f.label :{}, "{}" %>', { i(1, 'attribute'), i(0, 'Label') })),
+  s('render', fmt('<%= render "{}" %>', { i(0, 'partial') })),
+  s('renderw', fmt('<%= render "{}", {}: {} %>', { i(1, 'partial'), i(2, 'locals'), i(0, '@variable') })),
+  s('content', fmt('<%= content_tag :{}, {} do %>\n  {}\n<% end %>', { i(1, 'div'), i(2, 'class: "class-name"'), i(0) })),
+  s('image', fmt('<%= image_tag "{}", alt: "{}" %>', { i(1, 'path'), i(0, 'description') })),
+})
