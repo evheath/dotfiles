db8fafca2cd690e2b4fb0ce1a549d2e29adcfd97
Elliot Heath <elliot@businessfactory.dev>
2025-07-22 14:04:04 -0500

    erb comments

--- nvim/lua/custom/plugins/comment.lua
+++ nvim/lua/custom/plugins/comment.lua
@@ -1,7 +1,14 @@
 -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
 return {
   'numToStr/Comment.nvim',
-  opts = {
-    -- add any options here
-  },
+  config = function()
+    require('Comment').setup()
+
+    -- Set comment strings for ERB files to use HTML comments
+    local ft = require 'Comment.ft'
+    -- Use HTML comments for ERB files
+    ft.set('eruby', { '<%#%s%>', '<%#%s%>' })
+    ft.set('erb', { '<%#%s%>', '<%#%s%>' })
+    ft.set('embedded_template', { '<%#%s%>', '<%#%s%>' })
+  end,
 }
