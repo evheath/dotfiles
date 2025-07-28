07c62d3233895d2b20814a12f3fbd8682b8fef1b
Elliot Heath <elliot@businessfactory.dev>
2025-07-28 10:20:55 -0500

    snacks, explorer settings

--- nvim/lua/custom/plugins/snacks.lua
+++ nvim/lua/custom/plugins/snacks.lua
@@ -11,6 +11,21 @@ return {
     explorer = {
       enabled = true,
     },
+    picker = {
+      sources = {
+        explorer = {
+          auto_close = false, -- Don't auto-close when selecting files
+          jump = { close = false }, -- Don't close when jumping to files
+          layout = {
+            preset = 'sidebar',
+            layout = {
+              width = 30,
+              min_width = 20,
+            },
+          },
+        },
+      },
+    },
   },
   keys = {
     {
