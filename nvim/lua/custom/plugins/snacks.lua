c21654ae0d7bd25385b1e364695d6ce23a116084
Elliot Heath <elliot@businessfactory.dev>
2025-09-15 15:52:51 -0500

    scratch takes up most of editor

--- nvim/lua/custom/plugins/snacks.lua
+++ nvim/lua/custom/plugins/snacks.lua
@@ -1,12 +1,16 @@
--- https://github.com/folke/snacks.nvim/blob/main/docs/scratch.md
 return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
   ---@type snacks.Config
   opts = {
+    -- https://github.com/folke/snacks.nvim/blob/main/docs/scratch.md
     scratch = {
       ft = 'markdown',
+      win = {
+        width = 0.99,
+        height = 0.99,
+      },
     },
     explorer = {
       enabled = true,
@@ -14,8 +18,8 @@ return {
     picker = {
       sources = {
         explorer = {
-          auto_close = false, -- Don't auto-close when selecting files
-          jump = { close = false }, -- Don't close when jumping to files
+          -- auto_close = false, -- Don't auto-close when selecting files
+          -- jump = { close = false }, -- Don't close when jumping to files
           layout = {
             preset = 'sidebar',
             layout = {
