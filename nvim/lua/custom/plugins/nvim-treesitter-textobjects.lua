cb5da5d048099867678f9ed81466bc6b546f2b01
Elliot Heath <elliot@businessfactory.dev>
2026-01-23 11:01:35 -0600

    removing textobjects

--- nvim/lua/custom/plugins/nvim-treesitter-textobjects.lua
+++
@@ -1,6 +0,0 @@
--- allows for jumping to function signatures using tree sitter
--- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-return {
-  'nvim-treesitter/nvim-treesitter-textobjects',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-}
--- nvim/lua/custom/plugins/nvim-treesitter.lua
+++ nvim/lua/custom/plugins/nvim-treesitter.lua
@@ -1,27 +1,15 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- {[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'ruby' },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-    textobjects = {
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          [']f'] = '@function.outer',
-        },
-        goto_previous_start = {
-          ['[f'] = '@function.outer',
-        },
-      },
-    },
-  },
+  config = function()
+    require('nvim-treesitter').setup({
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'ruby' },
+      auto_install = true,
+    })
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+  end,
 }
