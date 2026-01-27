c7039a1f0c030777362b9cfa7006c3a81bc9dc62
Elliot Heath <elliot@businessfactory.dev>
2026-01-27 09:55:47 -0600

    displaying when recording and playing a macro

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -9,7 +9,17 @@ return {
       lualine_a = { 'mode' },
       lualine_b = { { 'filename', path = 1 } },
       lualine_c = { 'diagnostics' },
-      lualine_x = {},
+      lualine_x = { -- diplaying macro recording
+        {
+          function()
+            local reg = vim.fn.reg_recording()
+            if reg ~= '' then
+              return 'Recording @' .. reg
+            end
+            return ''
+          end,
+        },
+      },
       lualine_y = { 'progress' },
       lualine_z = { 'location' },
     },
--- nvim/lua/custom/plugins/nvim-notify.lua
+++ nvim/lua/custom/plugins/nvim-notify.lua
@@ -6,5 +6,15 @@ return {
       background_colour = '#1E1E2E',
       merge_duplicates = true,
     }
+
+    vim.keymap.set('n', '@', function()
+      local reg = vim.fn.getcharstr()
+      local count = vim.v.count1
+      vim.cmd('normal! ' .. count .. '@' .. reg)
+      local msg = count > 1
+        and string.format('Played macro @%s %d times', reg, count)
+        or string.format('Played macro @%s', reg)
+      vim.notify(msg, vim.log.levels.INFO)
+    end, { desc = 'Play macro with notification' })
   end,
 }
